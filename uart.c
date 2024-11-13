58d74b3955119662f2b17a3d457c5c878b121e50
pawan birnale <birnalepavan@gmail.com>
2024-11-13 22:07:30 +0530

    uart first commit

---
+++ uart.c
@@ -0,0 +1,15 @@
+/*
+ * File:   uart.c
+ * Author: PAWAN
+ *
+ * Created on 12 November, 2024, 1:13 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
